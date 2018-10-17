a0fa6f559f5de71a2f11297f3c83f938eac3b0a8
HADRIZIA VIRGINIA DE ARAUJO SANTOS 114110446 <hadriziavas@lcc3-063.lcc.ufcg.edu.br>
2018-10-17 17:37:58 -0300

    Adicionando componentes iniciais

---
+++ devweb/src/Timeline.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Post from './Post';
+import './Timeline.css';
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div className="Timeline">
+        <h1>Timeline</h1>
+        <Post/>
+      </div>
+    );
+  }
+}
+
+export default Timeline;
