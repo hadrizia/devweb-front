50c688dd825d62fe83c609aba5d41fb5c44e1d49
HADRIZIA VIRGINIA DE ARAUJO SANTOS 114110446 <hadriziavas@lcc3-066.lcc.ufcg.edu.br>
2018-10-31 14:16:32 -0300

    Adicionando componentes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Header from './Header';
+import Timeline from './Timeline';
+import './App.css';
+
+// código para quando o npm start nao funcionar: CHOKIDAR_USEPOLLING=true npm start
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header/>
+      <Timeline/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Avatar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Avatar.css';
+
+class Avatar extends Component {
+    render() {
+        return (
+          <div className="Avatar">
+            <p>COMENTARIOS</p>
+          </div>
+        );
+      }
+}
+
+export default Avatar;
