16614a01214696be0b878c1cacf750cb02b2dcd5
hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2018-12-05 17:27:11 -0300

    Primeira requisição com axios

--- src/App/Timeline/Timeline.js
+++ src/App/Timeline/Timeline.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import Post from './Post/Post';
 import './Timeline.css';
 
 class Timeline extends Component {
+
+  state = {
+      reports: []
+  }
+  
+  componentDidMount() {
+    axios.get(`http://localhost:8080/reports`)
+      .then(res => {
+        const reports = res.data.reports
+        this.setState({reports});
+      })
+  }
+
   render() {
     return (
       <div className="Timeline">
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+      { this.state.reports.map(report => 
+      <Post value={report} />)}
       </div>
     );
   }
